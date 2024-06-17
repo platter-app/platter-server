606aef2441ae84eb8780758549bef56525a10586
teemollt <teemollt@gmail.com>
2024-06-18 00:14:27 +0900

    service 분리 sign-in-with-provider 추가

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,206 +1,131 @@
-import { supabase } from "@/libs/supabase/client";
-import { zValidator } from "@/middlewares/zodValidator.middleware";
+// src/routes/auth.routes.ts
 
-import { db } from "@/libs/database/db";
-import { users } from "@/libs/database/schema";
 import { Hono } from "hono";
+import { zValidator } from "@/middlewares/zodValidator.middleware";
 import { getCookie, setCookie } from "hono/cookie";
 import { HTTPException } from "hono/http-exception";
-import { endTime, startTime } from "hono/timing";
+import { startTime, endTime } from "hono/timing";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import {
+  signUp,
+  signIn,
+  signInWithProvider,
+  refreshToken,
+} from "@/services/auth/auth.service";
 
-const authRoutes = new Hono()
-  .post(
-    "/sign-up",
-    zValidator(
-      "json",
-      z.object({
-        email: z.string(),
-        password: z.string().min(8),
-        // 닉네임 추가
-      })
-    ),
-    async (c) => {
-      const { email, password } = c.req.valid("json");
-      const { data, error } = await supabase.auth.signUp({
-        email,
-        password,
-      });
-      if (error || !data?.user?.email) {
-        if (
-          error?.message ===
-          'duplicate key value violates unique constraint "users_pkey"'
-        ) {
-          throw new HTTPException(422, {
-            message: "User already exists",
-          });
-        }
+const authRoutes = new Hono();
 
-        if (error?.code === "user_already_exists") {
-          throw new HTTPException(422, {
-            message: "User already exists",
-          });
-        }
+authRoutes.post(
+  "/sign-up",
+  zValidator(
+    "json",
+    z.object({
+      email: z.string().email(),
+      password: z.string().min(8),
+    })
+  ),
+  async (c) => {
+    const { email, password } = c.req.valid("json");
+    const user = await signUp(email, password);
+    return c.json(user);
+  }
+);
 
-        throw new Error(error?.message || "Error while signing up", {
-          cause: error,
-        });
-      }
+authRoutes.post(
+  "/sign-in",
+  zValidator(
+    "json",
+    z.object({
+      email: z.string().email(),
+      password: z.string().min(8),
+    })
+  ),
+  async (c) => {
+    const { email, password } = c.req.valid("json");
 
-      const dbUser = {
-        id: data?.user.id,
-        email: data?.user.email,
-        createdAt: data?.user.created_at,
-        updatedAt: data?.user.updated_at,
-      };
+    const data = await signIn(email, password);
 
-      const isUserExists = await db
-        .select()
-        .from(users)
-        .where(eq(users.id, dbUser.id));
-
-      if (isUserExists.length > 0) {
-        throw new HTTPException(422, {
-          message: "User already exists",
-        });
-      }
-
-      const user = await db.insert(users).values(dbUser).returning();
-
-      return c.json(user);
-    }
-  )
-  .post(
-    "/sign-in",
-    zValidator(
-      "json",
-      z.object({
-        email: z.string(),
-        password: z.string().min(8),
-      })
-    ),
-    async (c) => {
-      const { email, password } = c.req.valid("json");
-
-      const { data, error } = await supabase.auth.signInWithPassword({
-        email,
-        password,
-      });
-
-      if (error) {
-        if (error.message === "Invalid login credentials") {
-          throw new HTTPException(401, {
-            message: "Invalid login credentials",
-          });
-        }
-        throw new HTTPException(401, {
-          message: error.message,
-        });
-      }
-
-      setCookie(c, "access_token", data?.session.access_token, {
-        ...(data?.session.expires_at && {
-          expires: new Date(data.session.expires_at),
-        }),
-        httpOnly: true,
-        path: "/",
-        secure: true,
-      });
-
-      setCookie(c, "refresh_token", data?.session.refresh_token, {
-        ...(data?.session.expires_at && {
-          expires: new Date(data.session.expires_at),
-        }),
-        httpOnly: true,
-        path: "/",
-        secure: true,
-      });
-
-      return c.json(data.user);
-    }
-  )
-  .post(
-    "/sign-in-with-provider",
-    zValidator(
-      "json",
-      z.object({
-        provider: z.enum(["google", "apple"]),
-        token: z.string().min(8),
-        accessToken: z.string().optional(),
-      })
-    ),
-    async (c) => {
-      const { token, provider, accessToken } = c.req.valid("json");
-      // start a new timer
-      startTime(c, "supabase.auth.signInWithProvider");
-      const { data, error } = await supabase.auth.signInWithIdToken({
-        provider,
-        token,
-        access_token: accessToken,
-      });
-      // end the timer
-      endTime(c, "supabase.auth.signInWithProvider");
-
-      if (error) {
-        console.error("Error while signing in with Provider ", error);
-        throw new HTTPException(401, {
-          message: error.message,
-        });
-      }
-
-      setCookie(c, "access_token", data?.session.access_token, {
-        ...(data?.session.expires_at && {
-          expires: new Date(data.session.expires_at),
-        }),
-        httpOnly: true,
-        path: "/",
-        secure: true,
-      });
-
-      setCookie(c, "refresh_token", data?.session.refresh_token, {
-        ...(data?.session.expires_at && {
-          expires: new Date(data.session.expires_at),
-        }),
-        httpOnly: true,
-        path: "/",
-        secure: true,
-      });
-
-      return c.json(data.user);
-    }
-  )
-  .get("/refresh", async (c) => {
-    const refresh_token = getCookie(c, "refresh_token");
-    if (!refresh_token) {
-      throw new HTTPException(403, {
-        message: "No refresh token",
-      });
-    }
-
-    const { data, error } = await supabase.auth.refreshSession({
-      refresh_token,
+    setCookie(c, "access_token", data?.session.access_token, {
+      ...(data?.session.expires_at && {
+        expires: new Date(data.session.expires_at),
+      }),
+      httpOnly: true,
+      path: "/",
+      secure: true,
     });
 
-    if (error) {
-      console.error("Error while refreshing token", error);
-      throw new HTTPException(403, {
-        message: error.message,
-      });
-    }
-
-    if (data?.session) {
-      setCookie(c, "refresh_token", data.session.refresh_token, {
-        ...(data.session.expires_at && {
-          expires: new Date(data.session.expires_at),
-        }),
-        // httpOnly: true,
-        // path: "/",
-        // secure: true,
-      });
-    }
+    setCookie(c, "refresh_token", data?.session.refresh_token, {
+      ...(data?.session.expires_at && {
+        expires: new Date(data.session.expires_at),
+      }),
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
 
     return c.json(data.user);
-  });
+  }
+);
+
+authRoutes.post(
+  "/sign-in-with-provider",
+  zValidator(
+    "json",
+    z.object({
+      provider: z.enum(["google", "apple"]),
+      token: z.string().min(8),
+      accessToken: z.string().optional(),
+    })
+  ),
+  async (c) => {
+    const { token, provider, accessToken } = c.req.valid("json");
+
+    startTime(c, "supabase.auth.signInWithProvider");
+    const data = await signInWithProvider(provider, token, accessToken);
+    endTime(c, "supabase.auth.signInWithProvider");
+
+    setCookie(c, "access_token", data?.session.access_token, {
+      ...(data?.session.expires_at && {
+        expires: new Date(data.session.expires_at),
+      }),
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
+
+    setCookie(c, "refresh_token", data?.session.refresh_token, {
+      ...(data?.session.expires_at && {
+        expires: new Date(data.session.expires_at),
+      }),
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
+
+    return c.json(data.user);
+  }
+);
+
+authRoutes.get("/refresh", async (c) => {
+  const refresh_token = getCookie(c, "refresh_token");
+  if (!refresh_token) {
+    throw new HTTPException(403, { message: "No refresh token" });
+  }
+
+  const data = await refreshToken(refresh_token);
+
+  if (data?.session) {
+    setCookie(c, "refresh_token", data.session.refresh_token, {
+      ...(data.session.expires_at && {
+        expires: new Date(data.session.expires_at),
+      }),
+      httpOnly: true,
+      path: "/",
+      secure: true,
+    });
+  }
+
+  return c.json(data.user);
+});
 
 export default authRoutes;
--- src/services/auth/auth.service.ts
+++ src/services/auth/auth.service.ts
@@ -1,3 +1,5 @@
+// src/services/auth.service.ts
+
 import { supabase } from "@/libs/supabase/client";
 import { db } from "@/libs/database/db";
 import { users } from "@/libs/database/schema";
@@ -12,24 +14,15 @@ export const signUp = async (email: string, password: string) => {
 
   if (error || !data?.user?.email) {
     console.log(error?.message);
-    if (
-      error?.message ===
-      'duplicate key value violates unique constraint "users_pkey"'
-    ) {
-      throw new HTTPException(422, {
-        message: "User already exists",
-      });
+    if (error?.message === 'duplicate key value violates unique constraint "users_pkey"') {
+      throw new HTTPException(422, { message: "User already exists" });
     }
 
     if (error?.code === "user_already_exists") {
-      throw new HTTPException(422, {
-        message: "User already exists",
-      });
+      throw new HTTPException(422, { message: "User already exists" });
     }
 
-    throw new Error(error?.message || "Error while signing up", {
-      cause: error,
-    });
+    throw new Error(error?.message || "Error while signing up", { cause: error });
   }
 
   const dbUser = {
@@ -39,15 +32,10 @@ export const signUp = async (email: string, password: string) => {
     updatedAt: data?.user.updated_at,
   };
 
-  const isUserExists = await db
-    .select()
-    .from(users)
-    .where(eq(users.id, dbUser.id));
+  const isUserExists = await db.select().from(users).where(eq(users.id, dbUser.id));
 
   if (isUserExists.length > 0) {
-    throw new HTTPException(422, {
-      message: "User already exists",
-    });
+    throw new HTTPException(422, { message: "User already exists" });
   }
 
   const user = await db.insert(users).values(dbUser).returning();
@@ -63,28 +51,35 @@ export const signIn = async (email: string, password: string) => {
 
   if (error) {
     if (error.message === "Invalid login credentials") {
-      throw new HTTPException(401, {
-        message: "Invalid login credentials",
-      });
+      throw new HTTPException(401, { message: "Invalid login credentials" });
     }
-    throw new HTTPException(401, {
-      message: error.message,
-    });
+    throw new HTTPException(401, { message: error.message });
   }
 
   return data;
 };
 
 export const refreshToken = async (refresh_token: string) => {
-  const { data, error } = await supabase.auth.refreshSession({
-    refresh_token,
-  });
+  const { data, error } = await supabase.auth.refreshSession({ refresh_token });
 
   if (error) {
     console.error("Error while refreshing token", error);
-    throw new HTTPException(403, {
-      message: error.message,
-    });
+    throw new HTTPException(403, { message: error.message });
+  }
+
+  return data;
+};
+
+export const signInWithProvider = async (provider: 'google' | 'apple', token: string, accessToken?: string) => {
+  const { data, error } = await supabase.auth.signInWithIdToken({
+    provider,
+    token,
+    access_token: accessToken,
+  });
+
+  if (error) {
+    console.error(`Error while signing in with ${provider} provider`, error);
+    throw new HTTPException(401, { message: error.message });
   }
 
   return data;
